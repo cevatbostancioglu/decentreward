b05798e8411091df9bcf791df26c06ddfc2d2ad2
Cevat Bostancioglu <6525009+cevatbostancioglu@users.noreply.github.com>
2022-05-21 18:00:36 +0200

    contracts: indent fixed, candidates replaced with proof, proof location stored on ipfs, random and data request are sent to oracles at the same time, variable names are fixed

--- contracts/rewards.sol
+++ contracts/rewards.sol
@@ -48,10 +48,10 @@ contract DRewards is Ownable, ChainlinkClient {
 	// The default is 3, but you can set this higher.
 	uint16 requestConfirmations = 3;
 
+	// contest
 	uint256 public latestContestID = 1;
 
-	event TransferReceived(address _from, uint _amount);
-	event TransferSent(address _from, address _destAddr, uint _amount);
+	uint256 maxTwitterLikes = 1000000000; // 1e9
 
   	enum EContestState
    	{
@@ -59,113 +59,65 @@ contract DRewards is Ownable, ChainlinkClient {
 	   	CONTEST_CREATED, /* CONTEST ID, REWARDS, CONTEST OWNER */
 	   	CONTEST_RANDOM_REQUESTED, /* RANDOM REQUEST */
 	   	CONTEST_RANDOM_GENERATED, /* RANDOM GENERATED */
-	   	CONTEST_CANDIDATES_REQUESTED, /* candidates ids requested*/
-	   	CONTEST_CANDIDATES_DELIVERED, /* candidates ids delivered */
-	   	CONTEST_WINNER_LOTTERY_DONE, /* WINNER ANNOUNCED */
+	   	CONTEST_PROOF_REQUESTED, /* PROOF requested*/
+	   	CONTEST_PROOF_DELIVERED, /* PROOF delivered */
+		CONTEST_WINNER_LOTTERY_DONE, /* ETH DEPOSIT TO TWITTER WINNER ID */
 	   	CONTEST_REWARDS_DISTRIBUTED, /* REWARDS DISTRIBUTED */
 	   	CONTEST_END
    	}
 
    	// rewards
    	struct DReward {
-		uint256 contestID;    /* ID for indexing */
-	  	address contestOwner; /* eth address */
-	  	uint256 contestWinner;/* winnerIndex*/
-	  	uint256 rewardAmount; /* in eth for now*/
-	  	uint256 vrfRequestID; /* vrf request async handling*/
-	  	string  tweetID;      /* tweet id for contest      */
-	  	bytes32 tweetLikedByDataDeliveryRequestID; /* candidates */
-	  	uint256 randomSeed;   /* random seed from vrf/keccak */
-	  	EContestState contestState; /* state of contest */
-	  	uint256 rewardsDone;        /* not used. */
-		string[] candidatesIDs;     /* tweet liked by */
-   	}
+		uint256 contestID;    			/* ID for indexing */
+	  	
+		/* contest info */
+		address contestOwner; 			/* eth address */
+	  	uint256 rewardAmount; 			/* in eth for now*/
+		string  tweetID;      			/* tweet id for contest */
 
-   	mapping(uint256 => uint256) contestRandomTable;            // vrf requestID -> contest ID
-   	mapping(bytes32 => uint256) candidatesIDRequestTable;      // data requestID -> contest ID 
+		/* contest variables and proof */
+		uint256 randomSeed;   			/* random seed from vrf/keccak */
+	  	EContestState contestState; 	/* state of contest */
+		string  winnerTwitterID;     	/* winner Twitter ID */
+		string  ipfsLocation;        	/* ipfs Location */
+   	}
+	
+	mapping(uint256 => DReward) contest;
+
+	// vrf requestID -> contest ID
+   	mapping(uint256 => uint256) contestVRFRequestIDTable;
+	// data requestID -> contest ID 	
+   	mapping(bytes32 => uint256) dataRequestTable;
 
 	// userAddress => tokenAddress => token amount
    	mapping(address => mapping (address => uint256)) userDeposits;
+
+	// winnerTwitterID 
+	mapping(string => uint256) winnerEthAmount;
 
    	// userAdress => eth amount
    	mapping(address => uint) userEthDeposits; 
 
    	// userAddress <=> twitterID
-   	mapping(address => uint256) addressTwitterID;
-   	mapping(uint256 => address) twitterIDAdress;
+   	mapping(address => string) addressTwitterID;
+   	mapping(string => address) twitterIDAdress;
 
+	// constructor
    	constructor(address _link, address _oracle)
    	{
 		linkTokenAddress = _link;
 		setChainlinkToken(_link);
 		setChainlinkOracle(_oracle);
 	}
+
    	/*constructor(uint64 subscriptionId) VRFConsumerBaseV2(vrfCoordinator) {
 	  	COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
 	  	s_subscriptionId = subscriptionId;
    	}*/
 
-	// ERC20 all
-	event tokenDepositComplete(address tokenAddress, uint256 amount);
-	event tokenWithdrawComplete(address tokenAddress, uint256 amount);
 	event updateContestState(uint256 contestID, EContestState state);
 
-   	// eth deposit
-
-   	event ethDepositComplete(address user, uint amount);
-
-   	/* erc20 deposit disabled */
-	function approveToken(uint256 amount) public onlyOwner
-	{
-
-	}
-
-	/* erc20 deposit disabled */
-	function depositToken(uint256 amount) public onlyOwner
-	{
-		require(IERC20(linkTokenAddress).balanceOf(msg.sender) >= amount, 
-			"Your token amount must be greater then you are trying to deposit");
-
-		require(IERC20(linkTokenAddress).approve(address(this), amount));
-		require(IERC20(linkTokenAddress).transferFrom(msg.sender, address(this), amount));
-
-		userDeposits[msg.sender][linkTokenAddress] += amount;
-		emit tokenDepositComplete(linkTokenAddress, amount);
-	}
-
-	function depositEther() public payable returns(uint)
-	{
-		userEthDeposits[msg.sender] += msg.value;
-	  	emit ethDepositComplete(msg.sender, msg.value);
-	  	return userEthDeposits[msg.sender];
-	}
-
-   	function getEtherBalanceWithAdress(address user) public view returns(uint)
-   	{
-		return userEthDeposits[user];
-   	}
-
-	mapping(uint256 => DReward) contest;
-   
-  	function setTwitterID(address userAddress, uint256 twitterID)
-   		public
-   		onlyOwner
-   	{
-		require(twitterID > 0);
-		addressTwitterID[userAddress] = twitterID;
-		twitterIDAdress[twitterID] = userAddress;
-   	}
-
-   	function readTwitterID(address userAddress) public view returns(uint256)
-   	{
-		return addressTwitterID[userAddress];
-   	}
-
-   	function readAddressFromTwitterID(uint256 twitterID) public view returns(address)
-   	{
-		return twitterIDAdress[twitterID];
-   	}
-
+	// create new contest
   	function u_createNewContest(uint256 rewardAmount, string memory tweetID) 
 		public 
 		onlyEmptyContest(latestContestID) 
@@ -183,10 +135,11 @@ contract DRewards is Ownable, ChainlinkClient {
 	  	contest[latestContestID].contestOwner = msg.sender;
 	  	contest[latestContestID].contestID = latestContestID;
 	  	contest[latestContestID].tweetID = tweetID;
+	  	userEthDeposits[msg.sender] -= rewardAmount;
 	  	contest[latestContestID].rewardAmount += rewardAmount;
 	  	contest[latestContestID].contestState = EContestState.CONTEST_CREATED;
 	  	//userDeposits[msg.sender][linkTokenAddress] -= rewardAmount;
-	  	userEthDeposits[msg.sender] -= rewardAmount;
+		
 	  	emit updateContestState(latestContestID, contest[latestContestID].contestState);
 	  
 	  	latestContestID++;
@@ -198,6 +151,7 @@ contract DRewards is Ownable, ChainlinkClient {
 		public 
 		onlyOwner
 		onlyEmptyContest(latestContestID)
+		returns(uint256)
 	{
 		require(rewardAmount > 0);
 	  	require(userEthDeposits[contestOwner] <= rewardAmount, 
@@ -212,36 +166,103 @@ contract DRewards is Ownable, ChainlinkClient {
 		contest[latestContestID].rewardAmount += rewardAmount; // sum amount
 		contest[latestContestID].contestState = EContestState.CONTEST_CREATED;
 		//userDeposits[contestOwner][linkTokenAddress] -= rewardAmount;
-		latestContestID++;
 
 		emit updateContestState(latestContestID, contest[latestContestID].contestState);
+
+		latestContestID++;
+
+		return (latestContestID - 1);
    	}
-   
+
+	// trigger reward distro
+   	function triggerRewardDistrobution(uint256 contestID)
+		public
+	  	onlyValidContest(contestID)
+	  	onlyContestState(contestID, EContestState.CONTEST_PROOF_DELIVERED)
+   	{
+		// only user or owner.
+		contest[contestID].contestState = EContestState.CONTEST_WINNER_LOTTERY_DONE;
+		
+		winnerEthAmount[contest[contestID].winnerTwitterID] += contest[contestID].rewardAmount;
+
+		emit updateContestState(contestID, contest[contestID].contestState);
+   	}
+
+	// eth reward deposit
+   	event ethDepositComplete(address user, uint amount);
+	function depositEther() public payable returns(uint)
+	{
+		userEthDeposits[msg.sender] += msg.value;
+	  	emit ethDepositComplete(msg.sender, msg.value);
+	  	return userEthDeposits[msg.sender];
+	}
+
+	// ERC20 all
+	event tokenDepositComplete(address tokenAddress, uint256 amount);
+	
+   	/* erc20 deposit disabled */
+	/*function approveToken(uint256 amount) public onlyOwner
+	{
+
+	}*/
+
+	/* erc20 deposit disabled */
+	function depositToken(uint256 amount) public onlyOwner
+	{
+		require(IERC20(linkTokenAddress).balanceOf(msg.sender) >= amount, 
+			"Your token amount must be greater then you are trying to deposit");
+
+		require(IERC20(linkTokenAddress).approve(address(this), amount));
+		require(IERC20(linkTokenAddress).transferFrom(msg.sender, address(this), amount));
+
+		userDeposits[msg.sender][linkTokenAddress] += amount;
+		emit tokenDepositComplete(linkTokenAddress, amount);
+	}
+
+	// withdraw reward
+	function withdrawWinnerReward(uint256 contestID, address payable contestWinner)
+		public
+		onlyOwner
+		onlyValidContest(contestID)
+		onlyContestState(contestID, EContestState.CONTEST_WINNER_LOTTERY_DONE)
+	{
+		/*require(IERC20(linkTokenAddress).transfer(contestWinner, contest[contestID].rewardAmount), 
+			"transfer failed.");
+		*/
+		contest[contestID].contestState = EContestState.CONTEST_END;
+		string memory _winnerTwitterID = contest[contestID].winnerTwitterID;
+		contestWinner.transfer(winnerEthAmount[_winnerTwitterID]);
+		winnerEthAmount[_winnerTwitterID] = 0;
+		emit updateContestState(contestID, contest[contestID].contestState);
+	}
+
 	/**
 	* @notice Request variable bytes from the oracle
 	*/
-	function requestTweetLikesByTweetID(uint256 contestID)
+	function requestProofFromNode(uint256 contestID)
 		public
-	  	onlyContestState(contestID, EContestState.CONTEST_CREATED)
+		onlyOwner
+	  	onlyContestState(contestID, EContestState.CONTEST_RANDOM_GENERATED)
    	{
 		// only owner or user itself.
 		Chainlink.Request memory req = buildChainlinkRequest(specId, address(this), this.fulfillBytes.selector);
-		req.add("tweetID", contest[contestID].tweetID);
+		req.addUint("contestID", contestID);
+
 		bytes32 reqID = sendOperatorRequest(req, payment);
 
-		candidatesIDRequestTable[reqID] = contestID;
-		contest[contestID].contestState = EContestState.CONTEST_CANDIDATES_REQUESTED;
+		dataRequestTable[reqID] = contestID;
+		contest[contestID].contestState = EContestState.CONTEST_PROOF_REQUESTED;
    	}   
 
-   	event RequestFulfilled(
+	event RequestFulfilled(
 		bytes32 indexed requestId,
 	  	string[] indexed data
    	);
 
-   	/**
-		* @notice Fulfillment function for variable bytes
-		* @dev This is called by the oracle. recordChainlinkFulfillment must be used.
-		*/
+	/**
+	* @notice Fulfillment function for variable bytes
+	* @dev This is called by the oracle. recordChainlinkFulfillment must be used.
+	*/
 	function fulfillBytes(
 		bytes32 requestId,
 	 	string[] memory _data
@@ -249,8 +270,7 @@ contract DRewards is Ownable, ChainlinkClient {
 		public
 	  	recordChainlinkFulfillment(requestId)
    	{
-		emit RequestFulfilled(requestId, _data);
-	  	uint256 contestID = candidatesIDRequestTable[requestId];
+	  	uint256 contestID = getContestIDFromDataRequest(requestId);
 
 		/*
 		require(contestID > 0);
@@ -259,20 +279,97 @@ contract DRewards is Ownable, ChainlinkClient {
 		public onlyOwner
 			onlyContestState(contestID, EContestState.CONTEST_CREATED)
 		*/
-		contest[contestID].candidatesIDs = _data;
-		contest[contestID].contestState = EContestState.CONTEST_CANDIDATES_DELIVERED;
+		contest[contestID].winnerTwitterID = _data[0];
+		contest[contestID].ipfsLocation = _data[1];
+		contest[contestID].contestState = EContestState.CONTEST_PROOF_DELIVERED;
 		
+		triggerRewardDistrobution(contestID);
+
 		emit updateContestState(contestID, contest[contestID].contestState);
    	}
-   
-   	function getRewardAmount(uint256 contestID) 
+
+   	// VRF
+   	// Assumes the subscription is funded sufficiently.
+   	function requestRandomSeed(uint256 contestID) 
+		external 
+		onlyValidContest(contestID) 
+		onlyOwner
+		onlyContestState(contestID, EContestState.CONTEST_CREATED)
+	{ 
+	  	contest[contestID].randomSeed = uint256(keccak256(abi.encodePacked(block.timestamp,block.difficulty, msg.sender))) % maxTwitterLikes;
+		contest[contestID].contestState = EContestState.CONTEST_RANDOM_REQUESTED; /* vrf is not available now. */
+		contest[contestID].contestState = EContestState.CONTEST_RANDOM_GENERATED;
+		
+		requestProofFromNode(contestID);
+		
+		emit updateContestState(contestID, contest[contestID].contestState);
+		/* VRF have problems.
+		// Will revert if subscription is not set and funded.
+		contest[contestID].vrfRequestID = COORDINATOR.requestRandomWords(
+			keyHash,
+			s_subscriptionId,
+			requestConfirmations,
+			callbackGasLimit,
+			1
+		);
+		contestVRFRequestIDTable[contest[contestID].vrfRequestID] = contestID;
+		*/
+   	}
+
+   	function fulfillRandomWords(
+		uint256 requestID, /* requestId */
+		uint256[] memory randomWords
+   	) internal  
+   	{
+		/* override */
+		uint256 contestID = getContestIDFromVRFRequest(requestID);
+		contest[contestID].randomSeed = randomWords[0];
+		contest[contestID].contestState = EContestState.CONTEST_RANDOM_GENERATED;
+		emit updateContestState(contestID, contest[contestID].contestState);
+   	}
+
+	function setTwitterID(address userAddress, string memory twitterID)
+   		public
+   		onlyOwner
+   	{
+		require(bytes(twitterID).length > 0);
+		addressTwitterID[userAddress] = twitterID;
+		twitterIDAdress[twitterID] = userAddress;
+   	}
+
+	function getEtherBalanceWithAdress(address user) 
 		public 
 		view 
-		onlyValidContest(contestID) 
 		returns(uint256)
    	{
-		return contest[contestID].rewardAmount;
-  	}
+		return userEthDeposits[user];
+   	}
+
+	function getRewardBalanceWithTwitterID(string memory twitterID)
+		public
+		view
+		returns(uint256)
+	{
+		return winnerEthAmount[twitterID];
+	}
+	
+	function getContestIDFromVRFRequest(uint256 requestID) 
+		public
+		view
+		returns(uint256)
+	{
+		require(contestVRFRequestIDTable[requestID] != 0);
+		return contestVRFRequestIDTable[requestID];
+	}
+
+	function getContestIDFromDataRequest(bytes32 requestID)
+		public
+		view
+		returns(uint256)
+	{
+		require(dataRequestTable[requestID] != 0);
+		return dataRequestTable[requestID];
+	}
 
    	function getContestTwitterID(uint256 contestID)
 		public
@@ -281,14 +378,6 @@ contract DRewards is Ownable, ChainlinkClient {
 		returns(string memory)
    	{
 	  	return contest[contestID].tweetID;
-   	}
-   
-   	function getTotalNumberOfContest()
-		public
-		view
-		returns(uint256)
-   	{
-		return latestContestID;
    	}
 
    	function getRandomSeed(uint256 contestID) 
@@ -310,110 +399,46 @@ contract DRewards is Ownable, ChainlinkClient {
 		return contest[contestID].contestState;
    	}
 
-   	function getCandicates(uint256 contestID) 
+	function getTwitterID(address userAddress) public view returns(string memory)
+   	{
+		return addressTwitterID[userAddress];
+   	}
+
+   	function getAddressFromTwitterID(string memory twitterID) public view returns(address)
+   	{
+		return twitterIDAdress[twitterID];
+   	}
+
+   	function getProofLocation(uint256 contestID) 
 		public 
 	  	view 
 	  	onlyValidContest(contestID)
-	  	returns(string[] memory)
+	  	returns(string memory)
    	{
-		require(contest[contestID].contestState >= EContestState.CONTEST_CANDIDATES_DELIVERED);
-		return contest[contestID].candidatesIDs;
+		require(contest[contestID].contestState >= EContestState.CONTEST_PROOF_DELIVERED);
+		return contest[contestID].ipfsLocation;
    	}
    
-   	function triggerRewardDistrobution(uint256 contestID) 
-		public  
-	  	onlyValidContest(contestID)
-	  	onlyContestState(contestID, EContestState.CONTEST_RANDOM_GENERATED)
-   	{
-		// only user or owner.
-		uint256 winnerIndex = contest[contestID].randomSeed % contest[contestID].candidatesIDs.length;
-		
-		require(winnerIndex <= contest[contestID].candidatesIDs.length - 1);
-		
-		contest[contestID].contestState = EContestState.CONTEST_WINNER_LOTTERY_DONE;
-		//contest[contestID].candidatesIDs[winnerIndex];
-		contest[contestID].contestWinner = winnerIndex;
-		emit updateContestState(contestID, contest[contestID].contestState);
-   	}
-   
-   	function getWinnerID(uint256 contestID)
+   	function getWinnerTwitterID(uint256 contestID)
 		public
 		view
 		onlyValidContest(contestID)
-		returns(uint256)
+		returns(string memory)
 	{
-	  	require(contest[contestID].contestState >= EContestState.CONTEST_WINNER_LOTTERY_DONE); 
-	  	return contest[contestID].contestWinner;
+	  	require(contest[contestID].contestState >= EContestState.CONTEST_PROOF_DELIVERED); 
+	  	return contest[contestID].winnerTwitterID;
 	}
 
-	function getWinnerID2(uint256 contestID)
+	function getContestRewardAmount(uint256 contestID)
 		public
 		view
 		onlyValidContest(contestID)
 		returns(uint256)
 	{
-		require(contest[contestID].contestState >= EContestState.CONTEST_WINNER_LOTTERY_DONE); 
-		return contest[contestID].randomSeed % contest[contestID].candidatesIDs.length;;
-	}
-
-	function getWinnerRewardAmount(uint256 contestID)
-		public
-		view
-		onlyValidContest(contestID)
-		returns(uint256)
-	{
+		require(contest[contestID].contestState >= EContestState.CONTEST_CREATED);
+		require(contest[contestID].contestState <= EContestState.CONTEST_WINNER_LOTTERY_DONE);
 		return contest[contestID].rewardAmount;
 	}
-
-	function withdrawWinnerReward(uint256 contestID, address payable contestWinner)
-		public
-		onlyOwner
-		onlyValidContest(contestID)
-		onlyContestState(contestID, EContestState.CONTEST_WINNER_LOTTERY_DONE)
-	{
-		/*require(IERC20(linkTokenAddress).transfer(contestWinner, contest[contestID].rewardAmount), 
-			"transfer failed.");
-		*/
-		contest[contestID].contestState = EContestState.CONTEST_END;
-		contestWinner.transfer(contest[contestID].rewardAmount);
-		emit updateContestState(contestID, contest[contestID].contestState);
-	}
-
-   	// VRF
-   	// Assumes the subscription is funded sufficiently.
-   	function requestRandomSeed(uint256 contestID) 
-		external 
-		onlyValidContest(contestID) 
-		onlyOwner
-		onlyContestState(contestID, EContestState.CONTEST_CANDIDATES_DELIVERED)
-	{ 
-	  	contest[contestID].randomSeed = uint256(keccak256(abi.encodePacked(block.timestamp,block.difficulty, msg.sender)));
-		contest[contestID].contestState = EContestState.CONTEST_RANDOM_REQUESTED; /* vrf is not available now. */
-		contest[contestID].contestState = EContestState.CONTEST_RANDOM_GENERATED;
-		emit updateContestState(contestID, contest[contestID].contestState);
-		/* VRF have problems.
-		// Will revert if subscription is not set and funded.
-		contest[contestID].vrfRequestID = COORDINATOR.requestRandomWords(
-			keyHash,
-			s_subscriptionId,
-			requestConfirmations,
-			callbackGasLimit,
-			1
-		);
-		contestRandomTable[contest[contestID].vrfRequestID] = contestID;
-		*/
-   	}
-
-   	function fulfillRandomWords(
-		uint256 requestID, /* requestId */
-		uint256[] memory randomWords
-   	) internal  
-   	{
-		/* override */
-		contest[contestRandomTable[requestID]].randomSeed = randomWords[0];
-		contest[contestRandomTable[requestID]].contestState = EContestState.CONTEST_RANDOM_GENERATED;
-		emit updateContestState(contestRandomTable[requestID], contest[contestRandomTable[requestID]].contestState);
-   	}
 
    	modifier onlyEmptyContest(uint256 newContestID) {
 		require(newContestID > 0);
