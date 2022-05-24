cbb93c5ab1dec6a272441ed738fad4688423bfc9
Cevat Bostancioglu <6525009+cevatbostancioglu@users.noreply.github.com>
2022-05-24 23:11:59 +0200

    cl_node: typo's fixed.

--- cl_node/start.sh
+++ cl_node/start.sh
@@ -3,7 +3,7 @@
 set -x
 
 docker run --network=host \
-    -v ~/.chainlink-kovan:/chainlink \
+    -v ~/.chainlink-kovan/decentreward/:/chainlink \
     -it --env-file=.env \
     smartcontract/chainlink:1.3.0 \
     local n -p /chainlink/cl_node/.password -a /chainlink/cl_node/.api
