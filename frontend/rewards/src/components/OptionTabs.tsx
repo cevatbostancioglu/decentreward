bf9daf928bb704a297d2527cf7335b8155e096e8
Cevat Bostancioglu <6525009+cevatbostancioglu@users.noreply.github.com>
2022-05-27 05:53:22 +0200

    Frontend: documents tab added.

--- frontend/rewards/src/components/OptionTabs.tsx
+++ frontend/rewards/src/components/OptionTabs.tsx
@@ -19,37 +19,52 @@ export default function OptionTabs() {
 
     return (
         <Flex width="full" align="center" justifyContent="center">
-        <Tabs variant='soft-rounded' colorScheme='gray' align='center' isFitted mt={20}>
-        <TabList>
-            <Tab>Register</Tab>
-            <Tab>Deposit</Tab>
-            <Tab>Create</Tab>
-            <Tab>Status</Tab>
-            <Tab>Finish</Tab>
-            <Tab>Withdraw</Tab>
-        </TabList>
+        <Tabs variant='soft-rounded' colorScheme='gray' align='center' isFitted mt={20}> {/* main tab start */}
+            <TabList>
+                <Tab>Contest Management</Tab>
+                <Tab>Documents</Tab> 
+            </TabList>
 
-        <TabPanels >
-            <TabPanel>
-                <RegisterTab />
-            </TabPanel>
-            <TabPanel>
-                <DepositTab />
-            </TabPanel>
-            <TabPanel>
-                <CreateTab />
-            </TabPanel>
-            <TabPanel>
-                <StatusTab />
-            </TabPanel>
-            <TabPanel>
-                <FinishTab />
-            </TabPanel>
-            <TabPanel>
-                <WithdrawTab />
-            </TabPanel>
-        </TabPanels>
-        </Tabs>
+            <TabPanels>
+                <TabPanel> {/* mgmt tab */}
+                    <Tabs variant='soft-rounded' colorScheme='gray' align='center' isFitted mt={20}>
+                        <TabList>
+                            <Tab>Register</Tab>
+                            <Tab>Deposit</Tab>
+                            <Tab>Create</Tab>
+                            <Tab>Status</Tab>
+                            <Tab>Finish</Tab>
+                            <Tab>Withdraw</Tab>
+                        </TabList>
+
+                        <TabPanels >
+                            <TabPanel>
+                                <RegisterTab />
+                            </TabPanel>
+                            <TabPanel>
+                                <DepositTab />
+                            </TabPanel>
+                            <TabPanel>
+                                <CreateTab />
+                            </TabPanel>
+                            <TabPanel>
+                                <StatusTab />
+                            </TabPanel>
+                            <TabPanel>
+                                <FinishTab />
+                            </TabPanel>
+                            <TabPanel>
+                                <WithdrawTab />
+                            </TabPanel>
+                        </TabPanels>
+                    </Tabs>
+                </TabPanel> {/* end of mgmt tab */}
+                <TabPanel> {/* Documents tab */}
+                    
+                </TabPanel> {/* Documents tab */}
+            </TabPanels>
+        </Tabs> {/* main tab end */}
+        
         </Flex>
     );
 }
